b0996a8f575a400d41906360fb028fb9d7d9ac84
arthur <ar.popov@innopolis.university>
2025-09-09 13:32:52 +0300

    Add tests and tester

---
+++ src/test/java/o_compiler/LexicalAnalyzerTest.java
@@ -0,0 +1,51 @@
+package o_compiler;
+
+import org.junit.jupiter.api.Test;
+import org.o_compiler.LexicalAnalyzer.parser.TokenStream;
+
+import java.nio.file.*;
+import java.io.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LexicalAnalyzerTest {
+
+    private Path directory = Paths.get("src/test/resources/tests/lexicographical");
+
+    @Test
+    public void testCorrectFiles() throws IOException {
+        Files.list(directory)
+                .filter(Files::isRegularFile)
+                .filter(file -> !file.getFileName().toString().contains("not_correct"))
+                .forEach(file -> {
+                    try (InputStream inputStream = Files.newInputStream(file)) {
+                        System.out.println("Analyzing file: " + file.getFileName());
+                        assertTrue(analyzeLexicographically(inputStream), "File " + file.getFileName() + " should be correctly lexically structured.");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+    }
+
+    @Test
+    public void testNotCorrectFiles() throws IOException {
+        Files.list(directory)
+                .filter(Files::isRegularFile)
+                .filter(file -> file.getFileName().toString().contains("not_correct"))
+                .forEach(file -> {
+                    try (InputStream inputStream = Files.newInputStream(file)) {
+                        System.out.println("Analyzing file: " + file.getFileName());
+                        assertTrue(analyzeLexicographically(inputStream), "File " + file.getFileName() + " should be correctly lexically structured.");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+    }
+
+    public boolean analyzeLexicographically(InputStream inputStream) {
+        for (var token : new TokenStream(inputStream)) {
+            System.out.println(token);
+        }
+        return true;
+    }
+}
